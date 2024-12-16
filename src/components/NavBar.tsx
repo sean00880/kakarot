cebf81c195ef0865f2c913270aaae25674b0fe75
sean00880 <shantanu1997@gmail.com>
2024-12-16 09:21:39 -0500

    new5

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,15 @@
 "use client";
 import Link from "next/link";
-
+import Image from "next/image";
 export default function NavBar() {
   return (
     <nav className="fixed top-0 left-0 w-full bg-black/70 backdrop-blur-lg text-white z-50">
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
         {/* Logo */}
-        <div className="text-2xl font-bold">
-          <a href="#hero" className="hover:text-orange-400 transition">
-            Kakarot
-          </a>
+        <div className="text-2xl flex flex-row">
+          <a  href="#hero" className="hover:text-orange-400 transition"><h1>
+            Kakarot</h1><h3 className='text-white'><div className="flex flex-row"><Image alt='solana' src='/icons/solana.png' width={30} height={30}></Image> | SOL</div> </h3>
+        </a>
         </div>
 
         {/* Nav Links */}
@@ -32,11 +32,6 @@ export default function NavBar() {
           <li>
             <a href="#roadmap" className="hover:text-orange-400 transition">
               Roadmap
-            </a>
-          </li>
-          <li>
-            <a href="#footer" className="hover:text-orange-400 transition">
-              Footer
             </a>
           </li>
         </ul>
